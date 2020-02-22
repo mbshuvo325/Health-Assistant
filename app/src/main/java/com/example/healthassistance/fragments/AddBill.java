b1d138b27f50ce5f845d00a463101db55e05df6a
mbshuvo325 <mbshuvo2255@gmail.com>
2020-02-22 14:55:00 +0600

    version 0.8

---
+++ app/src/main/java/com/example/healthassistance/fragments/AddBill.java
@@ -0,0 +1,31 @@
+package com.example.healthassistance;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddBill extends Fragment {
+
+
+    public AddBill() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_bill, container, false);
+    }
+
+}
